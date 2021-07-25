3a19ba75dd203a65c9973c898a0f12764b1a7fbb
root <sevugan96mani@gmail.com>
2021-07-25 13:30:14 +0000

    new update

--- filecontent.rb
+++ filecontent.rb
@@ -3,7 +3,7 @@ status_file = '/vagrant/chef-repo/cookbooks/filecontent/recipes/newfile.txt'
 file status_file do
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0777'
   content 'Update from jenkins'
 end
 
